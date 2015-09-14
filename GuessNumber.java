4d533a11ffc8cb3460f3905694f2acc7e896bc9d
goloconda <flagrans@hotmail.com>
2015-09-14 09:21:08 +0200

    Ett programm att ändra som man vill i

--- GuessNumber.java
+++ GuessNumber.java
@@ -1,6 +1,48 @@
 public class GuessNumber {
-  public static void main(String[] args) {
-    // Ett program för att gissa nummer!
+  
+}import java.util.Random;
+import java.util.Scanner;
+ 
+public class GissaNummerMain {
+
+	public static int number, guess, maxValue;
+	public static Scanner scan = new Scanner(System.in);
+	public static Random rand;
+
+	public static void main(String[] args) {
+		System.out.println("Ett programm där man gissar vilket nummer som har slumpats fram inom ett givet spann.");
+
+		rand = new Random();
+		System.out.println("Skriv in det nummer du tycker ska vara det högsta möjliga nummret: ");
+		maxValue = scan.nextInt();
+		number = rand.nextInt(maxValue);
+
+		System.out.print("Skriv in din gissning: ");
+
+		while (guess != number) {
+			guess = scan.nextInt();
+			if (guess > number) {
+
+				System.out.println("Du gissade på ett för högt nummer");
+			} else if (guess < number) {
+				System.out.println("Du gissade på ett för lågt nummer");
+
+			}
+		}
+		System.out.println("Grattis, du har gissat rätt!");
+		
+//		System.out.println("Lägger in en loop för att kunna se förändringen i filen på GitHub");
+//		for(int i = 0; i < 10; i++){
+//			System.out.println("i = " + i );
+//		}
+//		
+//		System.out.println("Lägger in en förändring för att testa på GitHub");
+//		int i = 0; 
+//		System.out.println("Värdet på i är " + i);
+	}
+	
+	
+}
 
   }
 }
